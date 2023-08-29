8d367e603bfc9c1e34def930d95673af37949e7a
royalty1free <54598612+royalty1free@users.noreply.github.com>
2023-08-29 03:45:51 -0700

    Finished XLSX conversion
    Finished program that converts raw Tobii Export(.xlsx) to a readable .txt for the main analysis program

---
+++ src/main/java/analysis/xlsxToFxdTxt.java
@@ -0,0 +1,78 @@
+package analysis;
+
+import java.io.*;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class xlsxToFxdTxt {
+    /**
+     * Sums the duration of all GazeEvent Durations(46) from the xlsx
+     *
+     * @param wb
+     * @param rowIndex
+     * @return total fixation duration
+     */
+    public static int getDuration(XSSFWorkbook wb, int rowIndex) {
+        int x = 1;
+        if (wb.getSheetAt(0).getRow(rowIndex).getCell(46).getStringCellValue().equals("Unclassified")) {
+            System.out.println("Error: Wrong Starting Point");
+            return -1;
+        }
+        int duration = (int) (wb.getSheetAt(0).getRow(rowIndex).getCell(47).getNumericCellValue());
+        while (rowIndex + x < wb.getSheetAt(0).getLastRowNum() - 1 && wb.getSheetAt(0).getRow(rowIndex + x).getCell(46).getStringCellValue().equals("Unclassified")) {
+            duration += (int) (wb.getSheetAt(0).getRow(rowIndex + x).getCell(47).getNumericCellValue());
+            x++;
+        }
+        return duration;
+    }
+
+    /**
+     * Converts Tobii Export Data(.xlsx) to a readable format for the program
+     *
+     * @param inputFilePath
+     * @param outputFilePath
+     */
+    public static void convert(String inputFilePath, String outputFilePath) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath));
+            InputStream reader = new FileInputStream(inputFilePath);
+            XSSFWorkbook wb = new XSSFWorkbook(inputFilePath);
+            int fixationNumber, timestamp, eachFixationDuration, eachFixationX, eachFixationY;
+
+            //Retrieves all the necessary data from the .xlsx file and writes them to an output file
+            for (int x = 1; x < wb.getSheetAt(0).getLastRowNum(); x++) {
+                if (wb.getSheetAt(0).getRow(x).getCell(44).getCellType() != CellType.STRING) {
+                    if (wb.getSheetAt(0).getRow(x).getCell(46).getStringCellValue().equals("Fixation")) {
+                        if (!wb.getSheetAt(0).getRow(x).getCell(49).getStringCellValue().equals("")) {
+                            fixationNumber = (int) (wb.getSheetAt(0).getRow(x).getCell(44).getNumericCellValue());
+                            timestamp = (int) (wb.getSheetAt(0).getRow(x).getCell(26).getNumericCellValue());
+                            //eachFixationDuration = (int) (wb.getSheetAt(0).getRow(x).getCell(47).getNumericCellValue()); //Duration of one fixation
+                            eachFixationDuration = getDuration(wb, x);
+                            eachFixationX = Integer.parseInt(wb.getSheetAt(0).getRow(x).getCell(48).getStringCellValue());
+                            eachFixationY = Integer.parseInt(wb.getSheetAt(0).getRow(x).getCell(49).getStringCellValue());
+                            writer.write(fixationNumber + " " + timestamp + " " + eachFixationDuration + " " + eachFixationX + " " + eachFixationY);
+                            writer.newLine();
+                        }
+                    }
+                }
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //String inputFilePath = "input/Tobii Exports/ASD Raw Eye Tracking Data XLSX/ASD Eye Tracking_ASD Eye Tracking Data_SD1_013.xlsx";
+        String outputFilePath = "output/Tobii FXD Exports/";
+
+        File directoryPath = new File("input/Tobii Exports/ASD Raw Eye Tracking Data XLSX");
+        File[] files = directoryPath.listFiles();
+        for (File file : files) {
+            convert(file.getAbsolutePath(), outputFilePath + file.getName() + ".txt");
+        }
+
+        //convert(inputFilePath, outputFilePath);
+    }
+}
