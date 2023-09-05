7bb3b111ebd056c03d9dc990da030cb020030fe9
royalty1free <54598612+royalty1free@users.noreply.github.com>
2023-09-05 04:52:30 -0700

    Fixation Adjustment
    Changed Fixation output as csv from txt

--- src/main/java/analysis/fixation.java
+++ src/main/java/analysis/fixation.java
@@ -1,6 +1,6 @@
 package analysis;
 /*
- * Copyright (c) 2013, Bo Fu 
+ * Copyright (c) 2013, Bo Fu
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -38,61 +38,67 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
 
 public class fixation {
-	
-	public static void processFixation(String inputFile, String outputFile) throws IOException{
+
+    public static void processFixation(String inputFile, String outputFile) throws IOException {
 
         String line = null;
         ArrayList<Integer> allFixationDurations = new ArrayList<Integer>();
         ArrayList<Object> allCoordinates = new ArrayList<Object>();
         List<Point> allPoints = new ArrayList<Point>();
         ArrayList<Object> saccadeDetails = new ArrayList<Object>();
-        
+
         FileWriter fileWriter = new FileWriter(outputFile);
+        FileWriter outputFileWriter = new FileWriter(new File(outputFile));
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        CSVWriter outputCSVWriter = new CSVWriter(outputFileWriter);
 
         try {
             FileReader fileReader = new FileReader(inputFile);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            while((line = bufferedReader.readLine()) != null) {
-                
+            while ((line = bufferedReader.readLine()) != null) {
+
                 String[] lineArray = lineToArray(line);
-                
+
                 //get each fixation's duration
                 String eachFixationDuration = lineArray[2];
                 int eachDuration = Integer.parseInt(eachFixationDuration);
-                
+
                 //get each fixation's (x,y) coordinates
                 String eachFixationX = lineArray[3];
                 String eachFixationY = lineArray[4];
                 int x = Integer.parseInt(eachFixationX);
                 int y = Integer.parseInt(eachFixationY);
-                
-                Point eachPoint = new Point(x,y);
-                
+
+                Point eachPoint = new Point(x, y);
+
                 Integer[] eachCoordinate = new Integer[2];
                 eachCoordinate[0] = x;
                 eachCoordinate[1] = y;
-                
+
                 //get timestamp of each fixation
                 int timestamp = Integer.parseInt(lineArray[1]);
                 Integer[] eachSaccadeDetail = new Integer[2];
                 eachSaccadeDetail[0] = timestamp;
                 eachSaccadeDetail[1] = eachDuration;
-                
-                
+
+
                 allFixationDurations.add(eachDuration);
-                
+
                 allCoordinates.add(eachCoordinate);
-                
+
                 allPoints.add(eachPoint);
-                
+
                 saccadeDetails.add(eachSaccadeDetail);
-               
-            }	
-            
-            bufferedWriter.write("total number of fixations: " + getFixationCount(inputFile));
+
+            }
+
+            //Old Output Code
+            /*bufferedWriter.write("total number of fixations: " + getFixationCount(inputFile));
             bufferedWriter.newLine();
             
             bufferedWriter.write("sum of all fixation duration: " + descriptiveStats.getSumOfIntegers(allFixationDurations));
@@ -222,93 +228,239 @@ public class fixation {
             //Consider points in order, and discard those that would create a clockwise turn.
             List<Point> boundingPoints = convexHull.getConvexHull(allPoints);
             Point2D[] points = listToArray(boundingPoints);
-            
+
             bufferedWriter.write("convex hull area: " + convexHull.getPolygonArea(points));
             bufferedWriter.newLine();
             
             
-            bufferedWriter.close();
-            bufferedReader.close();	
-            System.out.println("done writing fixation data to: " + outputFile);
-            
-        }catch(FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + inputFile + "'");				
-        }catch(IOException ex) {
-            System.out.println("Error reading file '" + inputFile + "'");			
-        }
-	}
-	
-	public static String[] lineToArray(String lineOfData){
-		
-		StringTokenizer str = new StringTokenizer(lineOfData);
-		String[] values = new String[str.countTokens()];
+            */
 
-		while(str.hasMoreElements()) {
-			for(int i=0; i<values.length; i++){
-				values[i] = (String) str.nextElement();
-			}
-		}
-		
-		return values;
-	}
-	
-	public static Point2D[] listToArray(List<Point> allPoints){
-		Point2D[] points = new Point2D[allPoints.size()];
-        for(int i=0; i<points.length; i++){
-        	points[i] = allPoints.get(i);
+            //Code From Tobii.2150
+            ArrayList<String> headers = new ArrayList<>();
+            ArrayList<String> data = new ArrayList<>();
+
+            headers.add("total number of fixations");
+            data.add(String.valueOf(getFixationCount(inputFile)));
+
+            headers.add("sum of all fixation duration");
+            data.add(String.valueOf(descriptiveStats.getSumOfIntegers(allFixationDurations)));
+
+            headers.add("mean fixation duration (ms)");
+            data.add(String.valueOf(descriptiveStats.getMeanOfIntegers(allFixationDurations)));
+
+            headers.add("median fixation duration (ms)");
+            data.add(String.valueOf(descriptiveStats.getMedianOfIntegers(allFixationDurations)));
+
+            headers.add(" StDev of fixation durations (ms)");
+            data.add(String.valueOf(descriptiveStats.getStDevOfIntegers(allFixationDurations)));
+
+            headers.add("Min. fixation duration (ms)");
+            data.add(String.valueOf(descriptiveStats.getMinOfIntegers(allFixationDurations)));
+
+            headers.add("Max. fixation duration (ms)");
+            data.add(String.valueOf(descriptiveStats.getMaxOfIntegers(allFixationDurations)));
+
+            Double[] allSaccadeLengths = saccade.getAllSaccadeLength(allCoordinates);
+
+            headers.add("total number of saccades");
+            data.add(String.valueOf(allSaccadeLengths.length));
+
+            headers.add("sum of all saccade length");
+            data.add(String.valueOf(descriptiveStats.getSum(allSaccadeLengths)));
+
+            headers.add("mean saccade length");
+            data.add(String.valueOf(descriptiveStats.getMean(allSaccadeLengths)));
+
+
+            headers.add("median saccade length");
+            data.add(String.valueOf(descriptiveStats.getMedian(allSaccadeLengths)));
+
+            headers.add("StDev of saccade lengths");
+            data.add(String.valueOf(descriptiveStats.getStDev(allSaccadeLengths)));
+
+            headers.add("min saccade length");
+            data.add(String.valueOf(descriptiveStats.getMin(allSaccadeLengths)));
+
+
+            headers.add("max saccade length");
+            data.add(String.valueOf(descriptiveStats.getMax(allSaccadeLengths)));
+
+            ArrayList<Integer> allSaccadeDurations = saccade.getAllSaccadeDurations(saccadeDetails);
+
+            headers.add("sum of all saccade durations");
+            data.add(String.valueOf(descriptiveStats.getSumOfIntegers(allSaccadeDurations)));
+
+            headers.add("mean saccade duration");
+            data.add(String.valueOf(descriptiveStats.getMeanOfIntegers(allSaccadeDurations)));
+
+            headers.add("median saccade duration");
+            data.add(String.valueOf(descriptiveStats.getMedianOfIntegers(allSaccadeDurations)));
+
+
+            headers.add("StDev of saccade durations");
+            data.add(String.valueOf(descriptiveStats.getStDevOfIntegers(allSaccadeDurations)));
+
+            headers.add("Min. saccade duration");
+            data.add(String.valueOf(descriptiveStats.getMinOfIntegers(allSaccadeDurations)));
+
+            headers.add("Max. saccade duration");
+            data.add(String.valueOf(descriptiveStats.getMaxOfIntegers(allSaccadeDurations)));
+
+            headers.add("scanpath duration");
+            data.add(String.valueOf(getScanpathDuration(allFixationDurations, allSaccadeDurations)));
+
+
+            headers.add("fixation to saccade ratio");
+            data.add(String.valueOf(getFixationToSaccadeRatio(allFixationDurations, allSaccadeDurations)));
+
+            ArrayList<Double> allAbsoluteDegrees = angle.getAllAbsoluteAngles(allCoordinates);
+
+            headers.add("sum of all absolute degrees");
+            data.add(String.valueOf(descriptiveStats.getSumOfDoubles(allAbsoluteDegrees)));
+
+            headers.add("mean absolute degree");
+            data.add(String.valueOf(descriptiveStats.getMeanOfDoubles(allAbsoluteDegrees)));
+
+            headers.add("median absolute degree");
+            data.add(String.valueOf(descriptiveStats.getMedianOfDoubles(allAbsoluteDegrees)));
+
+            headers.add("StDev of absolute degrees");
+            data.add(String.valueOf(descriptiveStats.getStDevOfDoubles(allAbsoluteDegrees)));
+
+
+            headers.add("min absolute degree");
+            data.add(String.valueOf(descriptiveStats.getMinOfDoubles(allAbsoluteDegrees)));
+
+
+            headers.add("max absolute degree");
+            data.add(String.valueOf(descriptiveStats.getMaxOfDoubles(allAbsoluteDegrees)));
+
+
+            ArrayList<Double> allRelativeDegrees = angle.getAllRelativeAngles(allCoordinates);
+
+            headers.add("sum of all relative degrees");
+            data.add(String.valueOf(descriptiveStats.getSumOfDoubles(allRelativeDegrees)));
+
+            headers.add("mean relative degree");
+            data.add(String.valueOf(descriptiveStats.getMeanOfDoubles(allRelativeDegrees)));
+
+            headers.add("median relative degree");
+            data.add(String.valueOf(descriptiveStats.getMedianOfDoubles(allRelativeDegrees)));
+
+            headers.add("StDev of relative degrees");
+            data.add(String.valueOf(descriptiveStats.getStDevOfDoubles(allRelativeDegrees)));
+
+            headers.add("min relative degree");
+            data.add(String.valueOf(descriptiveStats.getMinOfDoubles(allRelativeDegrees)));
+
+
+            headers.add("max relative degree");
+            data.add(String.valueOf(descriptiveStats.getMaxOfDoubles(allRelativeDegrees)));
+
+            //getting the convex hull using Graham Scan
+            //i.e. Choose point p with smallest y-coordinate.
+            //Sort points by polar angle with p to get simple polygon.
+            //Consider points in order, and discard those that would create a clockwise turn.
+            List<Point> boundingPoints = convexHull.getConvexHull(allPoints);
+            Point2D[] points = listToArray(boundingPoints);
+
+            headers.add("convex hull area");
+            data.add(String.valueOf(convexHull.getPolygonArea(points)));
+
+            //Not Needed
+            /*headers.add("Average Peak Saccade Velocity");
+            data.add(avgPeakSaccadeVelocity(inputFile, outputFile));
+
+            headers.add("Average Blink Rate per Minute");
+            data.add(blinkRate(inputFile));*/
+
+            outputCSVWriter.writeNext(headers.toArray(new String[headers.size()]));
+            outputCSVWriter.writeNext(data.toArray(new String[data.size()]));
+            outputCSVWriter.close();
+
+            //            bufferedWriter.close();
+//            bufferedReader.close();
+
+            System.out.println("done writing fixation data to: " + outputFile);
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + inputFile + "'");
+        } catch (IOException ex) {
+            System.out.println("Error reading file '" + inputFile + "'");
+        }
+    }
+
+    public static String[] lineToArray(String lineOfData) {
+
+        StringTokenizer str = new StringTokenizer(lineOfData);
+        String[] values = new String[str.countTokens()];
+
+        while (str.hasMoreElements()) {
+            for (int i = 0; i < values.length; i++) {
+                values[i] = (String) str.nextElement();
+            }
+        }
+
+        return values;
+    }
+
+    public static Point2D[] listToArray(List<Point> allPoints) {
+        Point2D[] points = new Point2D[allPoints.size()];
+        for (int i = 0; i < points.length; i++) {
+            points[i] = allPoints.get(i);
         }
         return points;
-	}
-	
-	public static String getFixationCount(String inputFile) throws IOException {
-		File file = new File(inputFile); 
-	  
-	        RandomAccessFile fileHandler = new RandomAccessFile( file, "r" );
-	        long fileLength = file.length() - 1;
-	        StringBuilder sb = new StringBuilder();
+    }
 
-	        for(long filePointer = fileLength; filePointer != -1; filePointer--){
-	            fileHandler.seek( filePointer );
-	            int readByte = fileHandler.readByte();
+    public static String getFixationCount(String inputFile) throws IOException {
+        File file = new File(inputFile);
 
-	            if( readByte == 0xA ) {
-	                if( filePointer == fileLength ) {
-	                    continue;
-	                } else {
-	                    break;
-	                }
-	            } else if( readByte == 0xD ) {
-	                if( filePointer == fileLength - 1 ) {
-	                    continue;
-	                } else {
-	                    break;
-	                }
-	            }
+        RandomAccessFile fileHandler = new RandomAccessFile(file, "r");
+        long fileLength = file.length() - 1;
+        StringBuilder sb = new StringBuilder();
 
-	            sb.append( ( char ) readByte );
-	        }
+        for (long filePointer = fileLength; filePointer != -1; filePointer--) {
+            fileHandler.seek(filePointer);
+            int readByte = fileHandler.readByte();
 
-	        String lastLine = sb.reverse().toString();
-	        
-	        //get the first value in the last line, which is the total number of fixations
-	        String[] lineArray = lineToArray(lastLine);
-            String totalFixations = lineArray[0];
-	        
-	        return totalFixations;
-	 
-	}
-	
+            if (readByte == 0xA) {
+                if (filePointer == fileLength) {
+                    continue;
+                } else {
+                    break;
+                }
+            } else if (readByte == 0xD) {
+                if (filePointer == fileLength - 1) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
 
-	public static double getScanpathDuration(ArrayList<Integer> allFixationDurations, ArrayList<Integer> allSaccadeDurations){
-		double fixationDuration = descriptiveStats.getSumOfIntegers(allFixationDurations);
-		double saccadeDuration = descriptiveStats.getSumOfIntegers(allSaccadeDurations);
-		return fixationDuration + saccadeDuration;
-	}
-	
-	public static double getFixationToSaccadeRatio(ArrayList<Integer> allFixationDurations, ArrayList<Integer> allSaccadeDurations){
-		double fixationDuration = descriptiveStats.getSumOfIntegers(allFixationDurations);
-		double saccadeDuration = descriptiveStats.getSumOfIntegers(allSaccadeDurations);
-		return fixationDuration/saccadeDuration;
-	}
-	
+            sb.append((char) readByte);
+        }
+
+        String lastLine = sb.reverse().toString();
+
+        //get the first value in the last line, which is the total number of fixations
+        String[] lineArray = lineToArray(lastLine);
+        String totalFixations = lineArray[0];
+
+        return totalFixations;
+
+    }
+
+
+    public static double getScanpathDuration(ArrayList<Integer> allFixationDurations, ArrayList<Integer> allSaccadeDurations) {
+        double fixationDuration = descriptiveStats.getSumOfIntegers(allFixationDurations);
+        double saccadeDuration = descriptiveStats.getSumOfIntegers(allSaccadeDurations);
+        return fixationDuration + saccadeDuration;
+    }
+
+    public static double getFixationToSaccadeRatio(ArrayList<Integer> allFixationDurations, ArrayList<Integer> allSaccadeDurations) {
+        double fixationDuration = descriptiveStats.getSumOfIntegers(allFixationDurations);
+        double saccadeDuration = descriptiveStats.getSumOfIntegers(allSaccadeDurations);
+        return fixationDuration / saccadeDuration;
+    }
+
 }
